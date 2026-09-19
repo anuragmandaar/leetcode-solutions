e13e6c99efeee33eab65721fd9d05c289650c1a5
anuragmandaar <anuragmandaar@gmail.com>
2026-09-19 16:52:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.58%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -3,11 +3,11 @@ class Solution {
         long left = 1, right = num;
         while (left <= right) {
             long mid = (left + right) / 2;
-            if (mid * mid == num) return true; // check if mid is perfect square
-            if (mid * mid < num) { // mid is small -> go right to increase mid
+            if (mid * mid == num) return true; 
+            if (mid * mid < num) { 
                 left = mid + 1;
             } else {
-                right = mid - 1; // mid is large -> to left to decrease mid
+                right = mid - 1; 
             }
         }
         return false;

ee36de66055eba5f25a9913ccb533961600c3fd2
anuragmandaar <anuragmandaar@gmail.com>
2026-09-21 02:11:30 +0530

    Time: 3 ms (45.25%), Space: 42.6 MB (45.51%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -1,19 +1,27 @@
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
-        List<Integer> arr = new ArrayList<>();
-        for(int i=left; i<=right; i++){
-          char[] arr2 = String.valueOf(i).toCharArray(); 
-          int count =0; 
-          for (int j = 0; j < arr2.length; j++) {
-           int digit = arr2[j] - '0';
-           if (digit != 0 && i % digit == 0) {
-            count+=1;
-           }
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = left; i <= right; i++) {
+            int n = i;
+            boolean valid = true;
+
+            while (n > 0) {
+                int digit = n % 10;
+
+                if (digit == 0 || i % digit != 0) {
+                    valid = false;
+                    break;
+                }
+
+                n /= 10;
+            }
+
+            if (valid) {
+                ans.add(i);
+            }
         }
-        if (count==arr2.length){
-            arr.add(i);
-           }
-        }
-        return arr;
+
+        return ans;
     }
 }
